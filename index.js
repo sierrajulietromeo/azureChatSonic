af3ef63dc61a0e4775af798a96257d081311ddba
Steve Rich <steven.john.rich@gmail.com>
2023-01-19 11:18:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const sdk = require('api')('@writesonic/v2.2#4enbxztlcbti48j');
 
-sdk.auth('apikeyhere');
+sdk.auth('e4e664bc-fcda-42bf-8e4e-ac2beecce363');
 sdk.chatsonic_V2BusinessContentChatsonic_post({
   enable_google_results: 'true',
   enable_memory: false,
